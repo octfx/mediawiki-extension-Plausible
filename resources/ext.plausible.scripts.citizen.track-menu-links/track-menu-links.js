22720a84f26a5bc8057d1be33ba82637c3d8bd99
Hannes <hannes@octofox.de>
2021-03-11 10:41:05 +0100

    bug: Fix tracking menu links

--- resources/ext.plausible.scripts.citizen.track-menu-links/track-menu-links.js
+++ resources/ext.plausible.scripts.citizen.track-menu-links/track-menu-links.js
@@ -6,15 +6,10 @@
 		return;
 	}
 
-	document.querySelectorAll( '.mw-portal li a' ).forEach( function ( link ) {
+	document.querySelectorAll( '#mw-drawer-menu .mw-portal li a' ).forEach( function ( link ) {
 		link.addEventListener( 'click', function ( event ) {
-			event.preventDefault();
-			event.stopPropagation();
 			window.plausible( eventName, {
 				props: { href: event.target.href },
-				callback: function () {
-					window.location = event.target.href;
-				}
 			} );
 		} );
 	} );

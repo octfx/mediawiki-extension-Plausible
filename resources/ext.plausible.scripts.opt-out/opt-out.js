08acc4e7c2bc5a703e080bf055f07a1175e89751
Hannes <hannes@octofox.de>
2021-05-19 09:55:19 +0200

    feat: Make OptOut Button previewable

--- resources/ext.plausible.scripts.opt-out/opt-out.js
+++ resources/ext.plausible.scripts.opt-out/opt-out.js
@@ -1,51 +1,55 @@
 (function () {
-	const optOutButtons = document.querySelectorAll('.plausible-opt-out');
-	const plausibleKey = 'plausible_ignore';
+	const optOut = () => {
+		const optOutButtons = document.querySelectorAll('.plausible-opt-out'),
+			plausibleKey = 'plausible_ignore';
 
-	if (optOutButtons.length === 0) {
-		return;
-	}
-
-	const setInclude = (button) => {
-		window.localStorage.removeItem(plausibleKey);
-
-		button.innerText = mw.message('ext-plausible-exclude-visits').text();
-		button.classList.remove('plausible-opt-out__enabled');
-		button.classList.add('plausible-opt-out__disabled');
-	};
-
-	const setExclude = (button) => {
-		window.localStorage.setItem(plausibleKey, 'true');
-
-		button.innerText = mw.message('ext-plausible-include-visits').text();
-		button.classList.remove('plausible-opt-out__disabled');
-		button.classList.add('plausible-opt-out__enabled');
-	};
-
-	const toggleState = (event) => {
-		try {
-			const state = window.localStorage.getItem(plausibleKey);
-
-			if (state === 'true') {
-				setInclude(event.target);
-				mw.notify(mw.message('ext-plausible-notification-visits-included').text());
-			} else {
-				setExclude(event.target);
-				mw.notify(mw.message('ext-plausible-notification-visits-excluded').text());
-			}
-		} catch (e) {
+		if (optOutButtons.length === 0) {
+			return;
 		}
-	}
 
-	const trackingState = window.localStorage.getItem(plausibleKey);
+		const setInclude = (button) => {
+			window.localStorage.removeItem(plausibleKey);
 
-	optOutButtons.forEach(button => {
-		button.addEventListener('click', toggleState);
+			button.innerText = mw.message('ext-plausible-exclude-visits').text();
+			button.classList.remove('plausible-opt-out__enabled');
+			button.classList.add('plausible-opt-out__disabled');
+		};
 
-		if (trackingState === 'true') {
+		const setExclude = (button) => {
+			window.localStorage.setItem(plausibleKey, 'true');
+
 			button.innerText = mw.message('ext-plausible-include-visits').text();
 			button.classList.remove('plausible-opt-out__disabled');
 			button.classList.add('plausible-opt-out__enabled');
-		}
-	})
+		};
+
+		const toggleState = (event) => {
+			try {
+				const state = window.localStorage.getItem(plausibleKey);
+
+				if (state === 'true') {
+					setInclude(event.target);
+					mw.notify(mw.message('ext-plausible-notification-visits-included').text());
+				} else {
+					setExclude(event.target);
+					mw.notify(mw.message('ext-plausible-notification-visits-excluded').text());
+				}
+			} catch (e) {
+			}
+		};
+
+		const trackingState = window.localStorage.getItem(plausibleKey);
+
+		optOutButtons.forEach(button => {
+			button.addEventListener('click', toggleState);
+
+			if (trackingState === 'true') {
+				button.innerText = mw.message('ext-plausible-include-visits').text();
+				button.classList.remove('plausible-opt-out__disabled');
+				button.classList.add('plausible-opt-out__enabled');
+			}
+		});
+	};
+
+	mw.hook( 'wikipage.content' ).add( optOut );
 })();

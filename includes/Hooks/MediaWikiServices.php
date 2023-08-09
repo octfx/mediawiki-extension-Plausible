f94d1f33b19f63fd6abc8a4f5624b20e94d287a7
H. C. Kruse <hannes@octofox.de>
2023-08-09 11:02:35 +0200

    feat: Add Ext:PageViewService support

--- includes/Hooks/MediaWikiServices.php
+++ includes/Hooks/MediaWikiServices.php
@@ -2,63 +2,43 @@
 
 namespace MediaWiki\Extension\Plausible\Hooks;
 
-
 use ExtensionRegistry;
 use MediaWiki\Extension\PageViewInfo\CachedPageViewService;
+use MediaWiki\Extension\Plausible\PlausiblePageViewService;
 use MediaWiki\Hook\MediaWikiServicesHook;
 use MediaWiki\Logger\LoggerFactory;
 use ObjectCache;
 
 class MediaWikiServices implements MediaWikiServicesHook {
 
-    /**
-     * @inheritDoc
-     */
-    public function onMediaWikiServices( $services ) {
-        if(!ExtensionRegistry::getInstance()->isLoaded('PageViewInfo')) {
-            return;
-        }
+	/**
+	 * @inheritDoc
+	 */
+	public function onMediaWikiServices( $services ) {
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'PageViewInfo' ) ) {
+			return;
+		}
 
-        global $wgPageViewInfoGAProfileId,
-               $wgPageViewInfoGACredentialsFile,
-               $wgPageViewInfoGAWriteCustomMap,
-               $wgPageViewInfoGAReadCustomDimensions,
-               $wgPageViewApiMaxDays;
+		global $wgPageViewApiMaxDays;
 
-        $profileId = $wgPageViewInfoGAProfileId;
-        if ( !$profileId ) {
-            return;
-        }
-        $credentialsFile = $wgPageViewInfoGACredentialsFile;
-        $customMap = $wgPageViewInfoGAWriteCustomMap;
-        $readCustomDimensions = $wgPageViewInfoGAReadCustomDimensions;
-        $cache = ObjectCache::getLocalClusterInstance();
-        $logger = LoggerFactory::getInstance( 'PageViewInfoGA' );
-        $cachedDays = max( 30, $wgPageViewApiMaxDays );
+		$cache = ObjectCache::getLocalClusterInstance();
+		$logger = LoggerFactory::getInstance( 'PageViewInfoGA' );
+		$cachedDays = max( 30, $wgPageViewApiMaxDays );
 
-        $services->redefineService(
-            'PageViewService',
-            static function () use (
-                $credentialsFile,
-                $profileId,
-                $customMap,
-                $readCustomDimensions,
-                $cache,
-                $logger,
-                $cachedDays
-            ) {
-                $service = new GoogleAnalyticsPageViewService( [
-                    'credentialsFile' => $credentialsFile,
-                    'profileId' => $profileId,
-                    'customMap' => $customMap,
-                    'readCustomDimensions' => $readCustomDimensions,
-                ] );
+		$services->redefineService(
+			'PageViewService',
+			static function () use (
+				$cache,
+				$logger,
+				$cachedDays
+			) {
+				$service = new PlausiblePageViewService();
 
-                $cachedService = new CachedPageViewService( $service, $cache );
-                $cachedService->setCachedDays( $cachedDays );
-                $cachedService->setLogger( $logger );
-                return $cachedService;
-            }
-        );
-    }
+				$cachedService = new CachedPageViewService( $service, $cache );
+				$cachedService->setCachedDays( $cachedDays );
+				$cachedService->setLogger( $logger );
+				return $cachedService;
+			}
+		);
+	}
 }
--- includes/PlausiblePageViewService.php
+++ includes/PlausiblePageViewService.php
@@ -2,162 +2,276 @@
 
 namespace MediaWiki\Extension\Plausible;
 
+use Config;
+use Exception;
 use InvalidArgumentException;
+use JsonException;
 use MediaWiki\Extension\PageViewInfo\PageViewService;
 use MediaWiki\MediaWikiServices;
-use MultiHttpClient;
 use MWTimestamp;
+use Psr\Log\NullLogger;
 use StatusValue;
 use Title;
 
-class PlausiblePageViewService implements PageViewService
-{
+class PlausiblePageViewService implements PageViewService {
+	private Config $config;
 
-    private MultiHttpClient $client;
-    private \Config $config;
+	/** @var int UNIX timestamp of 0:00 of the last day with complete data */
+	protected $lastCompleteDay;
 
-    public function __construct()
-    {
-        $this->client = MediaWikiServices::getInstance()->getHttpRequestFactory()
-            ->createMultiClient( [ 'maxConnsPerHost' => 8, 'usePipelining' => true ] );
+	/** @var array Cache for getEmptyDateRange() */
+	protected $range;
 
-        $this->config = MediaWikiServices::getInstance()->getMainConfig();
-    }
+	public function __construct() {
+		$this->config = MediaWikiServices::getInstance()->getMainConfig();
 
-    /**
-     * @inheritDoc
-     */
-    public function supports($metric, $scope): bool
-    {
-        return in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) &&
-            in_array( $scope, [ self::SCOPE_ARTICLE, self::SCOPE_TOP, self::SCOPE_SITE ] );
-    }
+		// Skip the current day for which only partial information is available
+		$this->lastCompleteDay = strtotime( '0:0 1 day ago', MWTimestamp::time() );
 
-    /**
-     * @param Title[] $titles
-     * @param int $days The number of days.
-     * @param string $metric One of the METRIC_* constants.
-     * @return StatusValue A status object with the data. Its success property will contain
-     *    per-title success information.
-     */
-    public function getPageData(array $titles, $days, $metric = self::METRIC_VIEW)
-    {
-        if ( !in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) ) {
-            throw new InvalidArgumentException( 'Invalid metric: ' . $metric );
-        }
+		$this->logger = new NullLogger();
+	}
 
-        $metric = $metric === self::METRIC_UNIQUE ? 'visitors' : 'visits';
+	/**
+	 * @inheritDoc
+	 */
+	public function supports( $metric, $scope ): bool {
+		return in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) &&
+			in_array( $scope, [ self::SCOPE_ARTICLE, self::SCOPE_TOP, self::SCOPE_SITE ] );
+	}
 
-        $query = [
-            'site_id' => $this->config->get('PlausibleDomainKey'),
-            'period' => '1d',
-            'metrics' => $metric,
-        ];
+	/**
+	 * @param Title[] $titles
+	 * @param int $days The number of days.
+	 * @param string $metric One of the METRIC_* constants.
+	 * @return StatusValue A status object with the data. Its success property will contain
+	 *    per-title success information.
+	 */
+	public function getPageData( array $titles, $days, $metric = self::METRIC_VIEW ): StatusValue {
+		if ( !in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) ) {
+			throw new InvalidArgumentException( 'Invalid metric: ' . $metric );
+		}
 
-        $status = \Status::newGood();
-        $urls = [];
+		if ( $days <= 0 ) {
+			throw new InvalidArgumentException( 'Invalid days: ' . $days );
+		}
 
-        $client = MediaWikiServices::getInstance()->getHttpRequestFactory()->createMultiClient();
+		$status = StatusValue::newGood();
 
-        foreach ($titles as $title) {
-            $query['filters'] = sprintf('event:page==%s', $title->getLocalURL());
+		$urls = [];
+		$metric = $metric === self::METRIC_UNIQUE ? 'visitors' : 'pageviews';
+		$query = $this->makeBaseQuery( $metric, $days );
 
-            $urls[] = [
-                'method' => 'POST',
-                'url' => sprintf(
-                    '%s/api/v1/stats/timeseries%s',
-                    $this->config->get('PlausibleDomain'),
-                    http_build_query($query),
-                ),
-                'headers' => [
-                    'Authorization' => 'Bearer: ',
-                ]
-            ];
+		$client = MediaWikiServices::getInstance()->getHttpRequestFactory()->createMultiClient();
 
+		foreach ( $titles as $title ) {
+			$query['filters'] = sprintf( 'event:page==%s', $title->getLocalURL() );
 
-        }
+			$urls[] = [
+				'method' => 'GET',
+				'url' => sprintf(
+					'%s/api/v1/stats/timeseries?%s',
+					$this->config->get( 'PlausibleDomain' ),
+					http_build_query( $query ),
+				),
+				'headers' => [
+					'Authorization' => $this->getAuthHeaderValue(),
+				],
+			];
+		}
 
-        $data = $client->runMulti($urls);
+		try {
+			$data = $client->runMulti( $urls );
+		} catch ( Exception $e ) {
+			return StatusValue::newFatal( 'pvi-invalidresponse' );
+		}
 
-        $return = [];
+		$result = [];
 
-        foreach ($data as $i => $response) {
-            [ $code, $reason, $headers, $body, $error ] = $response['response'];
+		foreach ( $data as $i => $response ) {
+			[ $code, $reason, $headers, $body, $error ] = $response['response'];
+			$title = $titles[$i]->getPrefixedDBkey();
+			$result[$title] = $this->getEmptyDateRange( $days );
 
-            if ($code !== 200) {
-                $return[$titles[$i]->getPrefixedDBkey()] = null;
-            } else {
-                $body = json_decode($body, true);
-                $return[$titles[$i]->getPrefixedDBkey()] = $body['results'][$metric]['value'];
-            }
-        }
+			if ( $code == 200 ) {
+				try{
+					$body = json_decode( $body, true, 512, JSON_THROW_ON_ERROR );
 
-        return $status->setResult(true, $return);
-    }
+					foreach ( $body['results'] as $data ) {
+						$result[$title][$data['date']] = $data[$metric];
+					}
+				} catch ( JsonException $e ) {
+					continue;
+				}
+			}
+		}
 
-    /**
-     * @inheritDoc
-     */
-    public function getSiteData($days, $metric = self::METRIC_VIEW)
-    {
-        if ( !in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) ) {
-            throw new InvalidArgumentException( 'Invalid metric: ' . $metric );
-        }
+		return $status->setResult( true, $result );
+	}
 
-        $metric = $metric === self::METRIC_UNIQUE ? 'visitors' : 'visits';
+	/**
+	 * @inheritDoc
+	 */
+	public function getSiteData( $days, $metric = self::METRIC_VIEW ) {
+		if ( !in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) ) {
+			throw new InvalidArgumentException( 'Invalid metric: ' . $metric );
+		}
 
-        $query = http_build_query([
-            'site_id' => $this->config->get('PlausibleDomainKey'),
-            'period' => '30d',
-            'metrics' => $metric,
-        ]);
+		if ( $days <= 0 ) {
+			throw new InvalidArgumentException( 'Invalid days: ' . $days );
+		}
 
-        MediaWikiServices::getInstance()->getHttpRequestFactory()->post(
-            sprintf('%s/api/v1/stats/timeseries%s', $this->config->get('PlausibleDomain'), $query),
-            [
-                'headers' => [
-                    'Authorization' => 'Bearer: ',
-                ]
-            ]
-        );
-    }
+		$metric = $metric === self::METRIC_UNIQUE ? 'visitors' : 'pageviews';
 
-    /**
-     * @inheritDoc
-     */
-    public function getTopPages($metric = self::METRIC_VIEW)
-    {
-        if ( !in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) ) {
-            throw new InvalidArgumentException( 'Invalid metric: ' . $metric );
-        }
+		$query = http_build_query( $this->makeBaseQuery( $metric, $days ) );
 
-        $metric = $metric === self::METRIC_UNIQUE ? 'visitors' : 'visits';
+		$request = MediaWikiServices::getInstance()->getHttpRequestFactory()->create(
+			sprintf( '%s/api/v1/stats/timeseries?%s', $this->config->get( 'PlausibleDomain' ), $query ),
+			[
+				'headers' => [
+					'Authorization' => $this->getAuthHeaderValue(),
+				]
+			]
+		);
 
-        $query = http_build_query([
-            'site_id' => $this->config->get('PlausibleDomainKey'),
-            'period' => '1d',
-            'property' => 'event:page',
-            'metrics' => $metric,
-            'limit' => 10,
-        ]);
+		$request->setHeader( 'Authorization', $this->getAuthHeaderValue() );
 
-        MediaWikiServices::getInstance()->getHttpRequestFactory()->post(
-            sprintf('%s/api/v1/stats/breakdown%s', $this->config->get('PlausibleDomain'), $query),
-            [
-                'headers' => [
-                    'Authorization' => 'Bearer: ',
-                ]
-            ]
-        );
-    }
+		$status = $request->execute();
 
-    /**
-     * @inheritDoc
-     */
-    public function getCacheExpiry($metric, $scope): int
-    {
-        // data is valid until the end of the day
-        $endOfDay = strtotime( '0:0 next day', MWTimestamp::time() );
-        return $endOfDay - time();
-    }
+		if ( !$status->isOK() ) {
+			return $status->fatal( 'pvi-invalidresponse' );
+		}
+
+		try {
+			$result = json_decode( $request->getContent(), true, 512, JSON_THROW_ON_ERROR );
+		} catch ( JsonException $e ) {
+			$result = null;
+		}
+
+		$out = [];
+
+		foreach ( $result['results'] as $pageMetric ) {
+			$out[$pageMetric['date']] = $pageMetric[$metric];
+		}
+
+		$status->setResult( true, $out );
+
+		return $status;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getTopPages( $metric = self::METRIC_VIEW ) {
+		if ( !in_array( $metric, [ self::METRIC_VIEW, self::METRIC_UNIQUE ] ) ) {
+			throw new InvalidArgumentException( 'Invalid metric: ' . $metric );
+		}
+
+		$metric = $metric === self::METRIC_UNIQUE ? 'visitors' : 'pageviews';
+
+		$query = http_build_query( $this->makeBaseQuery( $metric, 1 ), [
+			'property' => 'event:page',
+			'limit' => 10,
+		] );
+
+		$request = MediaWikiServices::getInstance()->getHttpRequestFactory()->create( sprintf( '%s/api/v1/stats/breakdown?%s', $this->config->get( 'PlausibleDomain' ), $query ),
+			[
+				'headers' => [
+					'Authorization' => $this->getAuthHeaderValue(),
+				]
+			]
+		);
+
+		$request->setHeader( 'Authorization', $this->getAuthHeaderValue() );
+
+		$status = $request->execute();
+
+		if ( !$status->isOK() ) {
+			return $status->fatal( 'pvi-invalidresponse' );
+		}
+
+		try {
+			$result = json_decode( $request->getContent(), true, 512, JSON_THROW_ON_ERROR );
+		} catch ( JsonException $e ) {
+			$result = null;
+		}
+
+		$out = [];
+
+		foreach ( $result['results'] as $pageMetric ) {
+			$out[self::pageTitleForMW( $pageMetric['page'] )] = $pageMetric[$metric];
+		}
+
+		$status->setResult( true, $out );
+
+		return $status;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCacheExpiry( $metric, $scope ): int {
+		// data is valid until the end of the day
+		$endOfDay = strtotime( '0:0 next day', MWTimestamp::time() );
+		return $endOfDay - time();
+	}
+
+	/**
+	 * The pageview API omits dates if there is no data. Fill it with nulls to make client-side
+	 * processing easier.
+	 * @param int $days
+	 * @return array YYYY-MM-DD => null
+	 */
+	protected function getEmptyDateRange( $days ) {
+		if ( !$this->range ) {
+			$this->range = [];
+			// we only care about the date part, so add some hours to avoid errors when there is a
+			// leap second or some other weirdness
+			$end = $this->lastCompleteDay + 12 * 3600;
+			$start = $end - ( $days - 1 ) * 24 * 3600;
+			for ( $ts = $start; $ts <= $end; $ts += 24 * 3600 ) {
+				$this->range[gmdate( 'Y-m-d', $ts )] = null;
+			}
+		}
+		return $this->range;
+	}
+
+	/**
+	 * Builds the bearer header
+	 *
+	 * @return string
+	 */
+	private function getAuthHeaderValue(): string {
+		return sprintf( 'Bearer %s', $this->config->get( 'PlausibleApiKey' ) );
+	}
+
+	/**
+	 * Query params used in almost all requests
+	 *
+	 * @param string $metric
+	 * @param int $days
+	 * @return array
+	 */
+	private function makeBaseQuery( string $metric, int $days ): array {
+		return [
+			'site_id' => $this->config->get( 'PlausibleDomainKey' ),
+			'period' => 'custom',
+			'date' => sprintf(
+				'%s,%s',
+				gmdate( 'Y-m-d', strtotime( '0:0 ' . $days . ' day ago', MWTimestamp::time() ) ),
+				gmdate( 'Y-m-d', strtotime( '0:0 today', MWTimestamp::time() ) )
+			),
+			'metrics' => $metric,
+		];
+	}
+
+	/**
+	 * @param string $title
+	 * @return string title text converted MediaWiki-friendly
+	 */
+	protected static function pageTitleForMW( string $title ): string {
+		$title = preg_replace( '/ - [^-]+$/', '', $title );
+		$title = preg_replace( '/ /', '_', $title );
+
+		return ltrim( $title, '/' );
+	}
 }
